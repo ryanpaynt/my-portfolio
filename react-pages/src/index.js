0138af576aa275e89578112510994a7fb30659f4
Ryan <ryanpaynt@uncc.edu>
2021-07-20 08:59:36 -0400

    fixed css import

--- react-pages/src/index.js
+++ react-pages/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'materialize-css/dist/css/materialize.min.css';
+import './public/css/materialize.css'
+import './public/css/materialize.min.css'
 
 ReactDOM.render(
     <React.StrictMode>
